f9a2d68651f7fb2cec14ff1939a633a0ad09cafd
kimingi318 <kimingipeter318@gmail.com>
2026-01-16 16:52:18 +0300

    Auth configuration done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,50 @@
-import { Stack } from 'expo-router';
+import { Slot, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
+import { AuthContextProvider, useAuth } from "../context/authContext";
 
-export default function RootLayout(){
-  return <Stack>
-    <Stack.Screen name='LandingPage' options={{headerShown: false}} />
-    <Stack.Screen name='SignIn' options={{headerShown: false}} />
-    <Stack.Screen name='SignUp' options={{title: 'SignUp'}} />
-  </Stack>;
+export function RootLayout() {
+  const {isAuthenticating} = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if(typeof isAuthenticating === 'undefined')
+      return;
+    const inApp = segments[0] === "(tabs)";
+    if (isAuthenticating && !inApp) {
+      //redirect to HomeScreen
+      router.replace("/(tabs)/HomeScreen");
+    }
+    else if (isAuthenticating === false) {
+      //redirect to LandingPage
+      router.replace("/LandingPage");
+    }
+    else if (!isAuthenticating && inApp) {
+      //redirect to HomeScreen
+      router.replace("/SignIn");
+    }
+  }, [isAuthenticating]);{
+
+  }
+  return <Slot/>
+
+}
+
+
+export default function RootNavigator() {
+
+  return (
+    <AuthContextProvider>
+      <RootLayout />
+    </AuthContextProvider>
+    // <Stack screenOptions={{ headerShown: false }}>
+    //   <Stack.Protected guard={!!session}>
+    //     <Stack.Screen name="(tabs)" />
+    //   </Stack.Protected>
+
+    //   <Stack.Protected guard={!session}>
+    //     <Stack.Screen name="SignIn" />
+    //   </Stack.Protected>
+    // </Stack>
+  );
 }
