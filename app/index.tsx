16bc3c4f751002824ee8679efa923156bf74d904
kimingi318 <kimingipeter318@gmail.com>
2026-01-16 16:43:46 +0300

    splash screen for Auth purpose

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { ActivityIndicator, Text, View } from 'react-native';
+
+export default function Splash() {
+    return(
+        <View className="flex-1 justify-center items-center bg-white">
+            <ActivityIndicator size="large" color="#0C06B9" />
+            <Text className="mt-4 text-blue-700 text-lg font-semibold">Loading EduBridge...</Text>
+        </View>
+    )
+}
