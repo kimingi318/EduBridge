35ce8a3ecaa795b432fa8ddeef78e9479820b230
kimingi318 <kimingipeter318@gmail.com>
2026-01-19 12:38:09 +0300

    Auth set up completed for signOut

--- app/(tabs)/ForumScreen.tsx
+++ app/(tabs)/ForumScreen.tsx
@@ -1,15 +1,10 @@
-import { Link } from 'expo-router';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function ForumScreen() {
   return (
-    <View className='pt-10'>
+    <View className="pt-10">
       <Text>ForumScreen</Text>
-      <Link href='../LandingPage'>Back</Link>
     </View>
-  )
+  );
 }
-
-
-
--- app/(tabs)/ProfileScreen.tsx
+++ app/(tabs)/ProfileScreen.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { useAuth } from "../../context/authContext";
 
 export default function ProfileScreen() {
+  //to use user info add user inside the curly braces
+  const { signOut } = useAuth();
+  const handleSignOut = async () => {
+    await signOut();
+  };
+  // console.log("user info: ", user);
   return (
     <View className="pt-10">
       <Text>ProfileScreen</Text>
-      <Text
-      >
-        Sign Out
-      </Text>
+      <Text onPress={handleSignOut}>Sign Out</Text>
     </View>
   );
 }
