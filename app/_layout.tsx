57c4db7cc823f6be8624bcce0ceef4f050a13795
kimingi318 <kimingipeter318@gmail.com>
2026-01-19 12:39:54 +0300

    few changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,36 +3,30 @@ import { useEffect } from "react";
 import { AuthContextProvider, useAuth } from "../context/authContext";
 
 export function RootLayout() {
-  const {isAuthenticating} = useAuth();
+  const { isAuthenticating } = useAuth();
   const segments = useSegments();
   const router = useRouter();
 
   useEffect(() => {
-    if(typeof isAuthenticating === 'undefined')
-      return;
+    if (typeof isAuthenticating === "undefined") return;
     const inApp = segments[0] === "(tabs)";
     if (isAuthenticating && !inApp) {
       //redirect to HomeScreen
       router.replace("/(tabs)/HomeScreen");
-    }
-    else if (isAuthenticating === false) {
+    } else if (isAuthenticating === false) {
       //redirect to LandingPage
-      router.replace("/LandingPage");
-    }
-    else if (!isAuthenticating && inApp) {
-      //redirect to HomeScreen
       router.replace("/SignIn");
+    } else if (!isAuthenticating && inApp) {
+      //redirect to HomeScreen
+      router.replace("/SignUp");
     }
-  }, [isAuthenticating]);{
-
+  }, [isAuthenticating]);
+  {
   }
-  return <Slot/>
-
+  return <Slot />;
 }
 
-
 export default function RootNavigator() {
-
   return (
     <AuthContextProvider>
       <RootLayout />
