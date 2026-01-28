95625f8c2a195611dc2470c96b770d661d2f4da2
kimingi318 <kimingipeter318@gmail.com>
2026-01-29 00:25:05 +0300

    tried git ignore

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,8 +3,14 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import {
+    browserLocalPersistence,
+    getReactNativePersistence,
+    initializeAuth,
+} from "firebase/auth";
 import { collection, getFirestore } from "firebase/firestore";
+import { Platform } from "react-native";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,8 +26,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Use appropriate persistence based on platform
+const persistence =
+  Platform.OS === "web"
+    ? browserLocalPersistence
+    : getReactNativePersistence(AsyncStorage);
+
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
+  persistence,
 });
 
 export const db = getFirestore(app);
