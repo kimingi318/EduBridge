3674d60113cd30daffe4e53f4d3d08cc56826f32
kimingi318 <kimingipeter318@gmail.com>
2026-01-31 22:33:35 +0300

    Modified to accept profile modal

--- app/(tabs)/ProfileScreen.tsx
+++ app/(tabs)/ProfileScreen.tsx
@@ -1,21 +1,28 @@
-import ImageViewer from "@/components/ImageViewer";
+import Profile from "@/components/Profile";
 import { apiFetch } from "@/utils/api";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import { ImageBackground } from "expo-image";
-import * as ImagePicker from 'expo-image-picker';
 import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
 import {
+  Image,
+  Platform,
   ScrollView,
+  StyleSheet,
   Text,
   TouchableOpacity,
   View
 } from "react-native";
 import {
-  heightPercentageToDP as hp
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp
 } from "react-native-responsive-screen";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { useAuth } from "../../context/authContext";
+
+const API_BASE_URL = Platform.OS === "web"
+    ? "http://localhost:3000"
+    : "http://192.168.100.4:3000";
 
 export default function ProfileScreen() {
   const router = useRouter();
@@ -23,50 +30,30 @@ export default function ProfileScreen() {
   const handleSignOut = async () => {
     await signOut();
   };
-  // console.log("user info: ", user);
   const [name, setName] = useState("");
   const [Phone, setPhone] = useState("");
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
-  const placeholder = require('@/assets/images/student-dp.jpeg')
+  const[isModalVisible,setIsModalVisible]=useState<boolean>(false);
 
-  const pickImageAsync = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ['images'],
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      const uri = result.assets[0].uri;
-      setSelectedImage(uri);
-
-      await apiFetch("/api/profiles", {
-        method: "POST",
-        body: JSON.stringify({
-          name,
-          phone: Phone,
-          profileImage: uri,
-        }),
-      });
-    }
-  };
   useEffect(() => {
     const loadProfile = async () => {
       try {
-        const res = await apiFetch("/api/profiles/me");
+        const res = await apiFetch(`${API_BASE_URL}/api/profiles/me`,{
+          method: "GET",
+        });
         if (!res.ok) {
-          if (res.status === 404) {
-            console.log("No profile found - user is creating one for the first time");
-          } else {
-            console.warn("Failed to load profile: API returned status", res.status);
-          }
+          setIsModalVisible(true);
           return;
-        }
+        }else {
         const data = await res.json();
-        if (data) {
+          if (!data){
+            setIsModalVisible(true);
+            return;
+          }else{
+          setIsModalVisible(false);
           setName(data.name ?? "");
           setPhone(data.phone ?? "");
-          setSelectedImage(data.profile_image ?? undefined);
+          setSelectedImage(data.profile_image ?? undefined);}
         }
       } catch (apiError) {
         console.warn("Failed to load user profile:", apiError);
@@ -76,12 +63,16 @@ export default function ProfileScreen() {
     loadProfile();
   }, []);
 
-
+  const editProfile = ()=>{
+    setIsModalVisible(true);
+  }
 
   return (
-    <SafeAreaView>
+    <View className="flex-1">
       <ImageBackground source={require("../../assets/images/main-bg-img.jpg")}>
+        <StatusBar style="light" />
         <ScrollView showsVerticalScrollIndicator={false}>
+
           {/* ================= HEADER ================= */}
           <View
             style={{ height: hp("28%") }}
@@ -93,60 +84,56 @@ export default function ProfileScreen() {
             </TouchableOpacity>
 
             {/* Avatar */}
-            <TouchableOpacity onPress={pickImageAsync}>
-              <ImageViewer imgSource={placeholder} selectedImage={selectedImage} />
-            </TouchableOpacity>
-
+            <View>
+              <Image source={{uri:selectedImage}}/>
+            </View>
 
             {/* Name */}
             <Text className="text-white"
             >
-              { "John Doe"}
+              {""}
             </Text>
             <Text className="text-slate-200 text-xs mt-1">
-             {  "Aspiring Software Engineer"}
+              {""}
             </Text>
             <Text className="text-slate-300 text-xs">
-              {"Computer science"}· {"IV"}
+              {""}· {""}
             </Text>
           </View>
 
           {/* ================= CONTACT INFO ================= */}
-          <View className="px-4 mt-6 space-y-3">
-
-
+          <View
+            style={{ paddingHorizontal: wp(2), paddingTop: hp(1) }}
+            className="bg-gray-100 rounded-t-[30px]">
             {/* Email */}
-            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center shadow-sm">
+            <View className="flex-row justify-between items-center">
+              <Text style={styles.header}>Personal details</Text>
+              <TouchableOpacity onPress={editProfile}>
+                <Feather name="edit-2" size={16} color="#2563EB" />
+              </TouchableOpacity >
+            </View>
+            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center">
               <View className="bg-indigo-100 p-2 rounded-lg">
                 <Ionicons name="mail" size={18} color="#2563EB" />
               </View>
-
               <View className="ml-3 flex-1">
                 <Text className="text-xs text-slate-500">Email</Text>
                 <Text className="text-sm text-slate-900">{user?.email ?? ""}</Text>
               </View>
-
-              {/* <TouchableOpacity>
-                <Feather name="edit-2" size={16} color="#2563EB" />
-              </TouchableOpacity> */}
             </View>
             {/* username */}
-            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center shadow-sm">
+            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center">
               <View className="bg-indigo-100 p-2 rounded-lg">
                 <Ionicons name="mail" size={18} color="#2563EB" />
               </View>
-
               <View className="ml-3 flex-1">
                 <Text className="text-xs text-slate-500">username</Text>
                 <Text className="text-sm text-slate-900">{""}</Text>
               </View>
-
-              {/* <TouchableOpacity>
-                <Feather name="edit-2" size={16} color="#2563EB" />
-              </TouchableOpacity> */}
             </View>
+
             {/* phone */}
-            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center shadow-sm">
+            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center">
               <View className="bg-indigo-100 p-2 rounded-lg">
                 <Ionicons name="mail" size={18} color="#2563EB" />
               </View>
@@ -155,14 +142,10 @@ export default function ProfileScreen() {
                 <Text className="text-xs text-slate-500">Phone</Text>
                 <Text className="text-sm text-slate-900">{""}</Text>
               </View>
-
-              {/* <TouchableOpacity>
-                <Feather name="edit-2" size={16} color="#2563EB" />
-              </TouchableOpacity> */}
             </View>
 
             {/* Registration */}
-            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center shadow-sm">
+            <View className="bg-white rounded-xl p-4 mb-2 flex-row items-center">
               <View className="bg-indigo-100 p-2 rounded-lg">
                 <Ionicons name="id-card" size={18} color="#2563EB" />
               </View>
@@ -173,82 +156,86 @@ export default function ProfileScreen() {
                 </Text>
                 <Text className="text-sm text-slate-900">{user?.regNo ?? ""}</Text>
               </View>
-
-              <TouchableOpacity>
-                <Feather name="edit-2" size={16} color="#2563EB" />
-              </TouchableOpacity>
             </View>
+
+            {/* ================= MY FORUMS ================= */}
+            <Text style={styles.header}>My Forum</Text>
+            <View className="bg-white rounded-xl p-3 flex-row items-center">
+              <View className="w-10 h-10 rounded-full bg-slate-800 items-center justify-center">
+                <Text className="text-white text-xs">CS</Text>
+              </View>
+              <View className="ml-3">
+                <Text className="text-sm font-medium">
+                  Department of Computer Science
+                </Text>
+                <Text className="text-xs text-slate-500">
+                  Forum · Announcement & Discussion
+                </Text>
+              </View>
+            </View>
+
+            <Profile
+            isVisible={isModalVisible}
+            onClose={()=>setIsModalVisible(false)}
+            ></Profile>
+
+            {/* ================= MY CLASSES ================= */}
+            <Section title="My Classes">
+              {[
+                {
+                  title: "Decision Support Systems",
+                  lecturer: "Dr. Osero",
+                  students: 150,
+                },
+                {
+                  title: "Network Management",
+                  lecturer: "Dr. Mwathi",
+                  students: 90,
+                },
+                { title: "Seminars in CS", lecturer: "Dr. Jane", students: 150 },
+              ].map((item, index) => (
+                <ClassCard key={index} {...item} />
+              ))}
+            </Section>
+
+            {/* ================= TIMETABLE ================= */}
+            <Section title="Time Table">
+              <View
+                style={{ height: hp("14%") }}
+                className="bg-black rounded-xl items-center justify-center"
+              >
+                <Text className="text-white text-xs">4.2 Time Table Preview</Text>
+              </View>
+            </Section>
           </View>
 
-          {/* ================= MY FORUMS ================= */}
-          <Section title="My Forums">
-            <ForumCard />
-          </Section>
-
-          {/* ================= MY CLASSES ================= */}
-          <Section title="My Classes">
-            {[
-              {
-                title: "Decision Support Systems",
-                lecturer: "Dr. Osero",
-                students: 150,
-              },
-              {
-                title: "Network Management",
-                lecturer: "Dr. Mwathi",
-                students: 90,
-              },
-              { title: "Seminars in CS", lecturer: "Dr. Jane", students: 150 },
-            ].map((item, index) => (
-              <ClassCard key={index} {...item} />
-            ))}
-          </Section>
-
-          {/* ================= TIMETABLE ================= */}
-          <Section title="Time Table">
-            <View
-              style={{ height: hp("14%") }}
-              className="bg-black rounded-xl items-center justify-center"
-            >
-              <Text className="text-white text-xs">4.2 Time Table Preview</Text>
-            </View>
-          </Section>
-          <TouchableOpacity onPress={()=> router.push("/NotificationScreen")}>
-            <Text className="text-indigo-600 text-center my-6">add faculty</Text>
-          </TouchableOpacity>
 
         </ScrollView>
       </ImageBackground>
-    </SafeAreaView>
+    </View>
   );
 }
 
 const Section = ({ title, children }: any) => (
-  <View className="px-4 mt-6">
-    <Text className="text-base font-semibold text-slate-900 mb-3">{title}</Text>
+  <View >
+    <Text style={styles.header}>{title}</Text>
     <View className="space-y-3">{children}</View>
   </View>
 );
-const ForumCard = () => (
-  <View className="bg-white rounded-xl p-3 flex-row items-center shadow-sm">
-    <View className="w-10 h-10 rounded-full bg-slate-800 items-center justify-center">
-      <Text className="text-white text-xs">CS</Text>
-    </View>
-    <View className="ml-3">
-      <Text className="text-sm font-medium">
-        Department of Computer Science
-      </Text>
-      <Text className="text-xs text-slate-500">
-        Forum · Announcement & Discussion
-      </Text>
-    </View>
-  </View>
-);
+
 const ClassCard = ({ title, lecturer, students }: any) => (
-  <View style={{ height: hp(8)}} className="bg-white rounded-xl p-3 mb-2 shadow-sm">
+  <View style={{ height: hp(6) }} className="bg-white rounded-xl p-3 mb-2 ">
     <Text className="text-lg font-medium">{title}</Text>
     <Text className="text-xs text-slate-500">
       Lecturer · {lecturer} · {students} students
     </Text>
   </View>
 );
+const styles = StyleSheet.create({
+    header: {
+    fontSize: hp(2.5),
+    fontWeight: "bold",
+    marginBottom: hp(1),
+    marginTop: hp(1),
+  },
+});
