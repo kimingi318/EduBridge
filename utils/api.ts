afe6c2b1794fe43adaf0ba8a6abff70aad15f02b
kimingi318 <kimingipeter318@gmail.com>
2026-02-27 18:05:01 +0300

    Ip address change

--- utils/api.ts
+++ utils/api.ts
@@ -3,7 +3,7 @@ import { auth } from "../firebaseConfig";
 
 // Centralized API Base URL - ONLY change this in one place
 export const API_BASE_URL =
-  Platform.OS === "web" ? "http://localhost:3000" : "http://192.168.0.108:3000";
+  Platform.OS === "web" ? "http://localhost:3000" : "http://192.168.100.4:3000";
 
 export const apiFetch = async (url: string, options: RequestInit = {}) => {
   const token = await auth.currentUser?.getIdToken();
