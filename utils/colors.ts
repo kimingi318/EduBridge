0333128ea85cc7d73355302a492f220e1fc29fdd
kimingi318 <kimingipeter318@gmail.com>
2026-03-04 22:29:28 +0300

    added theme colors

---
+++ utils/colors.ts
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  background: "#ffffff",
+  surface: "#f9fafb",
+  card: "#ffffff",
+  text: "#111827",
+  subText: "#6b7280",
+  primary: "#2563eb",
+};
+
+export const darkTheme = {
+  background: "#000000",
+  surface: "#111827",
+  card: "#1f2937",
+  text: "#ffffff",
+  subText: "#9ca3af",
+  primary: "#3b82f6",
+};
