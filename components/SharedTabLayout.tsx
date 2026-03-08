4dd7fea3f7da48696dac97ad18c66cfac3c335a3
kimingi318 <kimingipeter318@gmail.com>
2026-03-08 21:10:05 +0300

    Modified

--- components/SharedTabLayout.tsx
+++ components/SharedTabLayout.tsx
@@ -6,7 +6,6 @@ import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 
-// StatusBar.setStatusBarBackgroundColor('#244A9F', true)
 
 const SharedTabLayout: React.FC = () => {
   const insets = useSafeAreaInsets();
