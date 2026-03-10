e9e595effbff1530275fefae366feacc5b10cfdc
kimingi318 <kimingipeter318@gmail.com>
2026-03-10 18:42:31 +0300

    Display of memo and events on home screen

--- app/(student)/(tabs)/HomeScreen.tsx
+++ app/(student)/(tabs)/HomeScreen.tsx
@@ -2,12 +2,14 @@ import ClassCarousel from "@/components/ClassCarousel";
 import ProfileAvatar from "@/components/ProfileAvatar";
 import SearchBar from "@/components/searchBar";
 import { useAuth } from '@/context/authContext';
+import { db } from "@/firebaseConfig";
 import { API_BASE_URL, apiFetch } from "@/utils/api";
 import { darkTheme, lightTheme } from "@/utils/colors";
 import { getTimeRemaining } from '@/utils/time';
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
+import { collection, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import {
   ScrollView,
@@ -29,6 +31,42 @@ export default function HomeScreen() {
   const [classes, setClasses] = useState<any[]>([]);
   const scheme = useColorScheme();
   const theme = scheme === "dark" ? darkTheme : lightTheme;
+  const [events, setEvents] = useState<any[]>([]);
+  const [memos, setMemos] = useState<any[]>([]);
+  const [activeEventTab, setActiveEventTab] = useState<"Academic" | "Social" | "Memos">("Academic");
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "events"), (snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setEvents(data);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "memos"), (snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMemos(data);
+    });
+
+    return unsubscribe;
+  }, []);
+  const previewEvents = events
+    .filter((event) =>
+      activeEventTab === "Memos"
+        ? false
+        : event.category === activeEventTab
+    )
+    .slice(0, 5);
+
+  const previewMemos = memos.slice(0, 5);
 
   useEffect(() => {
     if (!sessions.length) {
@@ -82,126 +120,176 @@ export default function HomeScreen() {
   }, [profile]);
 
 
-
   return (
-    <View style={{flex:1, backgroundColor: theme.background}}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          {/* HEADER */}
-          <View style={{marginTop: hp(4), backgroundColor: theme.background}} className="px-5 flex-1 items-center pb-8 ">
-            <View className="flex-row items-center justify-between">
-              <View style={{ paddingHorizontal: wp(2) }}
-                className="flex-row items-center ">
-                <ProfileAvatar
-                  imageUri={profile?.profile_image}
-                  fallbackImage={require("../../../assets/images/student-dp.jpeg")}
-                  dotColor="#22C55E"   // green
-                />
-                <View>
-                  <Text style={{ fontSize: hp(1.5),color:theme.text }} className="font-inter-bold">
-                    Good morning, {profile?.username || 'Student'}
-                  </Text>
-                  <Text style={{ fontSize: hp(1.5),color:theme.subText }} className="font-inter">
-                    {profile?.level || ''} {profile?.course_name ? `· ${profile.course_name}` : 'Go to Profile and update profile'}
-                  </Text>
-                </View>
-              </View>
-            </View>
-
-            {/* SEARCH */}
-            <SearchBar />
-          </View>
-
-          {/* CONTENT */}
-          <View style={{backgroundColor:theme.surface}} className=" -mt-6 rounded-t-[30px] px-2 pt-3">
-            {/* TODAY'S SCHEDULE */}
-            <SectionHeader title="Today’s Classes" right="TimeTable" />
-            <View>
-              {classes.length > 0 ? (
-                <ClassCarousel sessions={classes} role="student" />
-              ) : (
-                <Text style={{color: theme.subText}} className=" mt-2">No schedule Today</Text>
-              )}
-            </View>
-
-            {/* EVENTS */}
-            <SectionHeader
-              title="Events"
-              right={<Text style={{color:theme.text}} className="font-semibold" onPress={() => router.push("/NotificationScreen")}>
-                See all</Text>} />
-            <View>
-              <View className="flex-row justify-center gap-2 items-center mb-3">
-                <Text className="bg-edublue text-white px-4 py-1 rounded-full text-xs">
-                  Academic
+    <View style={{ flex: 1, backgroundColor: theme.background }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* HEADER */}
+        <View style={{ marginTop: hp(4), backgroundColor: theme.background }} className="px-5 flex-1 items-center pb-8 ">
+          <View className="flex-row items-center justify-between">
+            <View style={{ paddingHorizontal: wp(2) }}
+              className="flex-row items-center ">
+              <ProfileAvatar
+                imageUri={profile?.profile_image}
+                fallbackImage={require("../../../assets/images/student-dp.jpeg")}
+                dotColor="#22C55E"   // green
+              />
+              <View>
+                <Text style={{ fontSize: hp(1.5), color: theme.text }} className="font-inter-bold">
+                  Good morning, {profile?.username || 'Student'}
                 </Text>
-                <Text className="bg-edulightblue text-edublue px-4 py-1 rounded-full text-xs">
-                  Social
-                </Text>
-                <Text className="bg-edulightblue text-edublue px-4 py-1 rounded-full text-xs">
-                  Memos
+                <Text style={{ fontSize: hp(1.5), color: theme.subText }} className="font-inter">
+                  {profile?.level || ''} {profile?.course_name ? `· ${profile.course_name}` : 'Go to Profile and update profile'}
                 </Text>
               </View>
-
-              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                {/* Event Card */}
-                <View style={{backgroundColor: theme.card}} className="rounded-2xl w-64 mr-4 overflow-hidden shadow">
-                  <Image
-                    source={require("../../../assets/images/Graduation-event-img.jpg")}
-                    style={{ height: hp(12) }}
-                    placeholder={{ blurhash }}
-                    transition={500}
-                  />
-                  <View className="p-3">
-                    <Text className="text-green-600 text-xs">FRI 21 NOV</Text>
-                    <Text style={{color:theme.text}} className="font-bold">14th Graduation Ceremony</Text>
-                    <Text style={{color:theme.subText}} className="text-gray-500 text-sm">
-                      8am – 5pm · Chuka Pavilion
-                    </Text>
-                  </View>
-                </View>
-                <View className="bg-white rounded-2xl w-64 mr-4 overflow-hidden shadow">
-                  <Image
-                    source={require("../../../assets/images/vote-event-img.jpg")}
-                    style={{ height: hp(12) }}
-                    placeholder={{ blurhash }}
-                    transition={500}
-                  />
-                  <View className="p-3">
-                    <Text className="text-green-600 text-xs">FRI 21 NOV</Text>
-                    <Text className="font-bold">Elections</Text>
-                    <Text className="text-gray-500 text-sm">
-                      6am – 1pm · Chuka Pavilion
-                    </Text>
-                  </View>
-                </View>
-              </ScrollView>
-            </View>
-
-            {/* MEMOS */}
-            <SectionHeader
-              title="Annoucements"
-              right={<Text style={{color:theme.text}} className=" font-semibold"
-                onPress={() => router.push("/NotificationScreen")}>See all</Text>} />
-            <View className=" mb-10">
-              <Memo
-                color="red"
-                title="CUSA Elections Nominee Registration"
-                subtitle="Deadline · Tuesday, 13 January, 10:00 AM"
-              />
-
-              <Memo
-                color="green"
-                title="Online Gate-Pass Acquisition"
-                subtitle="Thursday, 8 January"
-              />
-
-              <Memo
-                color="blue"
-                title="CAT Sit-in COSC 442 DSS"
-                subtitle="Wednesday, 25 February · 3:00 PM S403"
-              />
             </View>
           </View>
-        </ScrollView>
+
+          {/* SEARCH */}
+          <SearchBar />
+        </View>
+
+        {/* CONTENT */}
+        <View style={{ backgroundColor: theme.surface }} className=" -mt-6 rounded-t-[30px] px-2 pt-3">
+          {/* TODAY'S SCHEDULE */}
+          <SectionHeader title="Today’s Classes" right="TimeTable" />
+          <View>
+            {classes.length > 0 ? (
+              <ClassCarousel sessions={classes} role="student" />
+            ) : (
+              <Text style={{ color: theme.subText }} className=" mt-2">No schedule Today</Text>
+            )}
+          </View>
+
+          {/* EVENTS */}
+          <SectionHeader
+            title="Events"
+            right={<Text style={{ color: theme.text }} className="font-semibold" onPress={() =>
+              router.push({
+                pathname: "/NotificationScreen",
+                params: { tab: activeEventTab === "Memos" ? "memos" : "events" },
+              })
+            }>
+              See all</Text>} />
+          <View>
+            <View className="flex-row justify-center gap-2 items-center mb-3">
+              {["Academic", "Social", "Memos"].map((tab) => (
+                <TouchableOpacity
+                  key={tab}
+                  onPress={() => setActiveEventTab(tab as any)}
+                  className={`px-4 py-1 rounded-full ${activeEventTab === tab
+                    ? "bg-edublue"
+                    : "bg-edulightblue"
+                    }`}
+                >
+                  <Text
+                    className={`text-xs ${activeEventTab === tab
+                      ? "text-white"
+                      : "text-edublue"
+                      }`}
+                  >
+                    {tab}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {activeEventTab === "Memos"
+                ? previewMemos.map((memo) => (
+                  <View
+                    key={memo.id}
+                    style={{ backgroundColor: theme.card }}
+                    className="rounded-2xl w-64 mr-4 overflow-hidden shadow p-3"
+                  >
+                    <Text className="text-xs text-green-600">
+                      {memo.createdAt?.seconds
+                        ? new Date(memo.createdAt.seconds * 1000)
+                          .toDateString()
+                          .toUpperCase()
+                        : ""}
+                    </Text>
+
+                    <Text
+                      style={{ color: theme.text }}
+                      className="font-bold"
+                    >
+                      {memo.title}
+                    </Text>
+
+                    <Text
+                      style={{ color: theme.subText }}
+                      className="text-sm"
+                      numberOfLines={2}
+                    >
+                      {memo.message}
+                    </Text>
+                  </View>
+                ))
+                : previewEvents.map((event) => (
+                  <View
+                    key={event.id}
+                    style={{ backgroundColor: theme.card }}
+                    className="rounded-2xl w-64 mr-4 overflow-hidden shadow"
+                  >
+                    {event.imageUrl && (
+                      <Image
+                        source={{ uri: event.imageUrl }}
+                        style={{ height: hp(12) }}
+                        placeholder={{ blurhash }}
+                        transition={500}
+                      />
+                    )}
+
+                    <View className="p-3">
+                      <Text className="text-green-600 text-xs">
+                        {event.eventDate}
+                      </Text>
+
+                      <Text
+                        style={{ color: theme.text }}
+                        className="font-bold"
+                      >
+                        {event.title}
+                      </Text>
+
+                      <Text
+                        style={{ color: theme.subText }}
+                        className="text-sm"
+                      >
+                        {event.location}
+                      </Text>
+                    </View>
+                  </View>
+                ))}
+            </ScrollView>
+          </View>
+
+          {/* MEMOS */}
+          <SectionHeader
+            title="Annoucements"
+            right={<Text style={{ color: theme.text }} className=" font-semibold"
+              onPress={() => router.push("/NotificationScreen")}>See all</Text>} />
+          <View className=" mb-10">
+            <Memo
+              color="red"
+              title="CUSA Elections Nominee Registration"
+              subtitle="Deadline · Tuesday, 13 January, 10:00 AM"
+            />
+
+            <Memo
+              color="green"
+              title="Online Gate-Pass Acquisition"
+              subtitle="Thursday, 8 January"
+            />
+
+            <Memo
+              color="blue"
+              title="CAT Sit-in COSC 442 DSS"
+              subtitle="Wednesday, 25 February · 3:00 PM S403"
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
