868cade6bb3612bcdf64802a6c404583f0c3ad25
kimingi318 <kimingipeter318@gmail.com>
2026-03-13 17:55:49 +0300

    converted to a shared messageScreen

--- app/(admin)/(tabs)/MessageScreen.tsx
+++ app/(admin)/(tabs)/MessageScreen.tsx
@@ -1,15 +1,2 @@
-import MsgHeader from "@/components/MsgHeader";
-import { ImageBackground, View } from "react-native";
-
-export default function MessageScreen() {
-  return (
-    <ImageBackground
-      source={require("../../../assets/images/main-bg-img.jpg")}
-      className="flex-1"
-    >
-      <View className="flex-1 bg-[#F5F7FB] ">
-        <MsgHeader />
-      </View>
-    </ImageBackground>
-  );
-}
+import MessageScreen from "@/components/SharedMessageScreen";
+export default MessageScreen;
--- app/(lecturer)/(tabs)/MessageScreen.tsx
+++ app/(lecturer)/(tabs)/MessageScreen.tsx
@@ -1,16 +1,2 @@
-import MsgHeader from "@/components/MsgHeader";
-import React from "react";
-import { ImageBackground, View } from "react-native";
-
-export default function MessageScreen() {
-  return (
-    <ImageBackground
-      source={require("../../../assets/images/main-bg-img.jpg")}
-      className="flex-1  pt-10"
-    >
-      <View className="flex-1 bg-[#F5F7FB] rounded-t-2xl">
-        <MsgHeader />
-      </View>
-    </ImageBackground>
-  );
-}
+import MessageScreen from "@/components/SharedMessageScreen";
+export default MessageScreen;
